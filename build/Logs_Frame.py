9084abf6364a3ee41db2346993952b903f4e24ea
Jose Luis Alamag <alamagjoseluis@gmail.com>
2022-12-15 02:20:51 +0800

    Commit No Logo

--- build/Logs_Frame.py
+++ build/Logs_Frame.py
@@ -8,6 +8,10 @@ from pathlib import Path
 # from tkinter import *
 # Explicit imports to satisfy Flake8
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from tkinter import *
+import tkinter.ttk as ttk
+import tkinter.font as tkfont
+import csv
 
 
 OUTPUT_PATH = Path(__file__).parent
@@ -18,12 +22,9 @@ def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
 
-window = Tk()
-
-window.geometry("1366x768")
-window.configure(bg = "#000000")
-
 def log_frame(window):
+    
+    
     canvas = Canvas(
         window,
         bg = "#000000",
@@ -38,15 +39,19 @@ def log_frame(window):
         match choice:
             case 'a': 
                 from MainMenu_Frame import mainmenu
+                Tframe.destroy()
                 mainmenu(window)
             case 'b':
                 from Park_Frame import park_frame
+                Tframe.destroy()
                 park_frame(window)
             case 'c':
                 from Unpark_Frame import unpark_frame
+                Tframe.destroy()
                 unpark_frame(window)
             case 'd':
                 from Login_Frame import login_frame
+                Tframe.destroy()
                 login_frame(window)
 
     canvas.place(x = 0, y = 0)
@@ -62,7 +67,7 @@ def log_frame(window):
         1081.0,
         125.0,
         anchor="nw",
-        text="MAIN MENU",
+        text="LOGS MENU",
         fill="#FFFFFF",
         font=("Inter ExtraBold", 40 * -1)
     )
@@ -147,10 +152,57 @@ def log_frame(window):
         image=image_image_2
     )
 
+    ###Logs Table
+    Tframe = Frame(window, height=684, width=1034)
+    Tframe.pack(padx=0,pady=84,side=LEFT)
 
+    #scroll bar
+    table_scroll = Scrollbar(Tframe)
+    table_scroll.pack(side=RIGHT, fill=Y)
 
+    table = ttk.Treeview(Tframe, yscrollcommand=table_scroll.set)
+    table.config(height=684)
+    table.pack()
 
+    #config scroll bar
+    table_scroll.config(command=table.yview)
+
+    style = ttk.Style()
+    style.configure("Treeview.Heading", font=('Arial', 20))
+    style.configure("Treeview", font=('Arial 20'))
+    style.configure("Treeview", background='White', rowheight = 50)
+
+    #defining table colums
+    table['columns'] = ("Timestamp","Lot Number", "Parking", "Counter")
+    table.column('#0', minwidth=0, width=0)
+    table.column('Timestamp', minwidth=0, width=410)
+    table.column('Lot Number', minwidth=0, width=210, anchor=CENTER)
+    table.column('Parking', minwidth=0, width=200, anchor=CENTER)
+    table.column('Counter', minwidth=0, width=200, anchor=CENTER)
+
+    #Create headings
+    table.heading('Timestamp', text="Time Stamp", anchor=W)
+    table.heading('Lot Number', text="Lot Number", anchor=CENTER)
+    table.heading('Parking', text="Parking", anchor=CENTER)
+    table.heading('Counter', text="Counter", anchor=CENTER)
+
+    with open('C:\\Users\\alama\\OneDrive\\Desktop\\ModSim Parking\\ParkingLogs.csv') as f:
+        reader = csv.DictReader(f, delimiter=',')
+        for row in reader:
+            timestamp = row['timestamp']
+            lot_num = row['lot_num']
+            in_out = row['in_out']
+            parked_counter = row['parked_counter']
+            table.insert("", 0, values=(timestamp, lot_num, in_out, parked_counter))
+
+    
     window.resizable(False, False)
     window.mainloop()
 
-log_frame(window)
+
+'''window = Tk()
+
+window.geometry("1366x768")
+window.configure(bg = "#000000")
+
+log_frame(window)'''
--- build/MAIN.py
+++ build/MAIN.py
@@ -3,10 +3,13 @@ from tkinter import *
 def main():
     window = Tk()
 
-    window.geometry("1366x768")
+    ''''window.geometry("1366x768")'''
+    w, h = window.winfo_screenwidth(), window.winfo_screenheight()
+    window.geometry("%dx%d+0+0" % (w, h))
     window.configure(bg = "#000000")
     from Login_Frame import login_frame
-
+    
+    window.resizable(True, True)
     login_frame(window)
 
 main()
